7e4da71452b9f50bf99db051bbb0266490639aaf
James Wang <wang.james.j@gmail.com>
2025-10-03 10:50:34 -0700

    Added --verbose flag

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -13,13 +14,21 @@ content_request = sys.argv
 if len(content_request) < 2:
     print("No prompt provided.")
     sys.exit(1)
+
+messages = [
+    types.Content(role="user", parts=[types.Part(text=content_request[1])]),
+]
     
 
 response = client.models.generate_content(
     model="gemini-2.0-flash-001", 
-    contents=content_request[1]
+    contents=messages,
     )
 
 print(response.text)
-print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+
+if "--verbose" in sys.argv:
+    print(f"User prompt: {content_request[1]}")
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
