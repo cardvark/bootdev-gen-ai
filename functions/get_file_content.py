dd6a18fb3d13151a849a389a1e7bccc5012e9a0f
James Wang <wang.james.j@gmail.com>
2025-10-06 12:22:26 -0700

    Added file read functionality.

---
+++ functions/get_file_content.py
@@ -0,0 +1,28 @@
+import os
+from functions.config import *
+
+def get_file_content(working_directory, file_path):
+    full_path = os.path.join(working_directory, file_path)
+    abs_full_path = os.path.abspath(full_path)
+
+    print(full_path)
+
+    if not abs_full_path.startswith(os.path.abspath(working_directory)):
+        print(f"Error: Cannot read '{file_path}' as it is outside the permitted working directory")
+        return
+
+    if not os.path.isfile(full_path):
+        print(f"Error: File not found or is not a regular file:'{file_path}'")
+        return
+
+    try:
+        with open (full_path, 'r') as file:
+            file_content = file.read(FILE_LENGTH_LIMIT)
+            # print(file_content)
+            if len(file_content) == FILE_LENGTH_LIMIT:
+                print(f"[...File '{file_path}' truncated at {FILE_LENGTH_LIMIT} characters]")
+    except Exception as e:
+        print(f"Error: {e}")
+        
+    
+    return file_content
